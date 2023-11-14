0b5d5f7641be441810271bd2bee3bd838e8cf1a0
Sean <G00420811@atu.ie>
2023-11-14 13:51:04 +0000

    added feature 2 with the try and catch method added function to let the user keep retrying

--- src/main/java/week7/ie/atu/Main.java
+++ src/main/java/week7/ie/atu/Main.java
@@ -7,27 +7,29 @@ import java.util.Scanner;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        try{
-           System.out.println("Please enter a number between 0 - 9 : ");
-           Scanner myscan = new Scanner(System.in);
+        int x=1;
+        while(x==1) {
+            try {
+                System.out.println("Please enter a number between 0 - 9 : ");
+                Scanner myscan = new Scanner(System.in);
 
-           //read user input as a string
-            String input = myscan.nextLine();
+                //read user input as a string
+                String input = myscan.nextLine();
 
-            //parse the input to an interger
-            int myNum = Integer.parseInt(input);
+                //parse the input to an interger
+                int myNum = Integer.parseInt(input);
 
-            //check if the number is between 0 and 9
+                //check if the number is between 0 and 9
                 if (myNum >= 0 && myNum <= 9) {
-                    System.out.println("You entered " + myNum);
+                    System.out.println("You entered " + myNum + "\n");
+                    x=2;
                 }
-                else{
-                    System.out.println("Not a valid number");
+                else {
+                    System.out.println("Not a valid number\n");
                 }
-        }
-        catch(NumberFormatException e){
-            System.out.println("Enter a valid numeric value");
+            } catch (NumberFormatException e) {
+                System.out.println("Enter a valid numeric value\n");
             }
-
+        }
     }
 }
