b9af8cd6cd9304a16c56da497d4b8f835ff08e1e
Sean <G00420811@atu.ie>
2023-11-14 13:11:07 +0000

    initial init

---
+++ src/main/java/week7/ie/atu/Main.java
@@ -0,0 +1,16 @@
+package week7.ie.atu;
+
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+       System.out.println("Please enter a number between 0 - 9 : ");
+       Scanner myscan = new Scanner(System.in);
+
+       //read user input as a string
+        String input = myscan.nextLine();
+        }
+    }
+}
